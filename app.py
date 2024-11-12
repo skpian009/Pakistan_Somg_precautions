b79c54187c71f0aff21f58ec1e9ebe086b3dd580
Zubair Ghaffar <zubairee57@gmail.com>
2024-11-12 18:22:48 +0500

    app.py

--- app.py
+++ app.py
@@ -118,8 +118,8 @@ else:
 
 #st.write(f"File exists: {os.path.exists(DATA_FILE)}")
 
- load_dotenv()
- secret_code = os.getenv("CODE1")
+load_dotenv()
+secret_code = os.getenv("CODE1")
 # Temporary debug output (remember to remove after confirming)
 #st.write("Debug Secret Code:", secret_code)
 
