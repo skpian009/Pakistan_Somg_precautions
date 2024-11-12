342c028d4e673925937b4885c722e7c7e8c9d719
Zubair Ghaffar <zubairee57@gmail.com>
2024-11-12 15:06:34 +0500

    app.py

--- app.py
+++ app.py
@@ -118,11 +118,11 @@ else:
 
 #st.write(f"File exists: {os.path.exists(DATA_FILE)}")
 # Access the secret code from GitHub Secrets (environment variable)
-# load_dotenv()
-#secret_code = os.getenv("CODE1")
-st.write(st.secrets)
-secret_code = st.secrets["CODE1"]  # Access directly from st.secrets
-st.write("Secret Code (for debugging):", secret_code)
+load_dotenv()
+secret_code = os.getenv("CODE1")
+#st.write(st.secrets)
+#secret_code = st.secrets["CODE1"]  # Access directly from st.secrets
+#st.write("Secret Code (for debugging):", secret_code)
 
 # Temporary debug output (remember to remove after confirming)
 #st.write("Debug Secret Code:", secret_code)
